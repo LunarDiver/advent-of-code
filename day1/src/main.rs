9a575353ab6e86c1c5620cdc48f470d666a31ba9
Lunardiver <dev@lunardiver.com>
2024-10-27 16:51:29 +0100

    Added basic file check

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error, fs};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let mut args = std::env::args_os().map(|arg| arg.into_string());
+    args.next(); //first argument is always command that started this app
+    let filename = args
+        .next()
+        .ok_or("No file specified to process")?
+        .or(Err("Invalid filename"))?;
+
+    println!("{}", fs::exists(&filename).unwrap());
+
+    Ok(())
 }
